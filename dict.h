e2492e38d3fed36447cdf4051fbaa59ef2df907d
Yannayd <58879756+Yannayd@users.noreply.github.com>
2019-12-15 13:55:53 +0200

    Update dict.h

--- dict.h
+++ dict.h
@@ -1,8 +1,19 @@
 #ifndef _DICT_H_
 #define _DICT_H_
-
 #include "hash.h"
 
-// Add your declarations here...
+typedef struct _Element_Word* pWord;
+
+
+//-------------------------------------------part 1 function-------------------------------------------
+int HashWord(char* word, int size);
 
 #endif
+
+//-------------------------------------------struct word fucntion
+pWord createElementWord(char* word, char* translate);
+char* getElementInTranslate(pWord pw);
+char* getElementInKey(pWord pw);
+char* getElementInWord(pWord pw);
+Result setElementTranslate(pWord pw, char* translate);
+Result setElementWordAndKey(pWord pw, char* word);
